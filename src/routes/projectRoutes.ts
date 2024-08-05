02a56c4c3f54c34bee3a7173d0aa4910bcf84f96
Emiliano Tancredi <111023349+EmilianoTancredi21@users.noreply.github.com>
2024-08-05 19:11:23 -0300

    Instalamos express validator para validar los campos del modelo antes de hacer una petición POST, se crea un archivo validation que luego se utiliza en projectRoutes, de esta forma nos aseguramos de que todo este ordenado correctamente.

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,7 +1,17 @@
 import { Router } from "express";
 import { ProjectController } from "../controllers/ProjectController";
+import {body} from "express-validator"
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
+
+router.post("/",
+body("projectName").notEmpty().withMessage("El nombre del proyecto es obligatorio"),
+body("clientName").notEmpty().withMessage("El nombre del cliente es obligatorio"),
+body("description").notEmpty().withMessage("La descripción del proyecto es obligatoria"),
+handleInputErrors,
+ProjectController.createProject);
+
 
 router.get("/", ProjectController.getAllProjects );
 
