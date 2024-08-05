07d9e5e15291f84049911777e48783874070607e
Emiliano Tancredi <111023349+EmilianoTancredi21@users.noreply.github.com>
2024-08-05 19:53:37 -0300

    Se añadió el método updateProject para actualizar proyectos en la base de datos.

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -20,4 +20,12 @@ param("id").isMongoId().withMessage("Id no válido"),
 handleInputErrors,
 ProjectController.getAllProjectsById);
 
+router.put("/:id", 
+param("id").isMongoId().withMessage("Id no válido"),
+body("projectName").notEmpty().withMessage("El nombre del proyecto es obligatorio"),
+body("clientName").notEmpty().withMessage("El nombre del cliente es obligatorio"),
+body("description").notEmpty().withMessage("La descripción del proyecto es obligatoria"),
+handleInputErrors,
+ProjectController.updateProject);
+
 export default router;
